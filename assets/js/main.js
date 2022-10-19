dc3c3757cf2c653236bde67ed2c88afcebda8861
fabio98p <parisifabio98@gmail.com>
2022-10-19 17:12:59 +0200

    aggiunto il fatto di card che si girano

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
+// flip card source https://codepen.io/mondal10/pen/WNNEvjV
 let res = ""
 const cards = [] 
-var firstId
+let firstCard
+
 class Card {
-    constructor(id, link, name) {
+    constructor(id, typeId, link, name) {
         this.id = id
+        this.typeId = typeId
         this.link = link;
         this.name = name;
     }
@@ -40,41 +43,57 @@ const cardsType = [
         "name": "Tram"
     }
 ]
-cardsType.forEach( card => {
-    cards.push(new Card(card.id, card.link, card.name))
-    cards.push(new Card(card.id, card.link, card.name))
-})
 
+let counter = 0
+cardsType.forEach( card => {
+    cards.push(new Card(counter++, card.id, card.link, card.name))
+    cards.push(new Card(counter++, card.id, card.link, card.name))
+})
+counter = 0 
 // shuffle(cards)
 cards.forEach(card => {
+    // <div class="container ${card.name}" onclick="selectCard('${encodeURIComponent(JSON.stringify(card))}')">
+    //     <div class="card_fix">
+    //     <div class="position_name">
+    //         ${card.name}
+    //     </div>
+    //     </div>
+    //     <div class="pattern"></div>
+    // </div>
     res = `${res}
-    <div class="container ${card.name}" onclick="selectCard(${card.id}, '${card.name}')">
-        <div class="card_fix">
-        <div class="position_name">
-            ${card.name}
+
+    <div class="scene scene--card">
+        <div class="card ${card.name} ${card.name}${card.id}" onclick="selectCard('${encodeURIComponent(JSON.stringify(card))}')">
+            <div class="card__face card__face--front">image</div>
+            <div class="card__face card__face--back">${card.name}</div>
         </div>
-        </div>
-        <div class="pattern"></div>
     </div>
+
   `
 });
 document.getElementById("cards").innerHTML = res
 
-function selectCard(id, name){
+function selectCard(card){
+    card = JSON.parse(decodeURIComponent(card))
+    console.log(card);
     debugger
-    if (!firstId) {
-        firstId = id
+    if (!firstCard) {
+        firstCard = card
+        document.getElementsByClassName(`${card.name}${card.id}`)[0].classList.add('is-flipped')
     }else{
-        if(firstId == id){
-            
-            var els = document.getElementsByClassName(name)
-            Array.prototype.forEach.call(els, function(el) {
-                el.classList.add("active")
-            });
+        if (firstCard.Id != card.id) {
+            if(firstCard.typeId == card.typeId){
+                
+                var els = document.getElementsByClassName(`${card.name}`)
+                Array.prototype.forEach.call(els, function(el) {
+                    el.classList.add("active")
+                });
+            }
         }
-        firstId = null
+        firstCard.typeId = null
+        firstCard.Id = null
+        document.getElementsByClassName(`${card.name}${card.id}`)[0].classList.remove('is-flipped')
     }
-console.log(id);
 }
 
 function shuffle(array) {
